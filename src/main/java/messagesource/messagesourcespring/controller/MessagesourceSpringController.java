3c4173527fed76435ee9369c6e5efa2c31d9b0d0
leyuri <leyuri97@gmail.com>
2021-01-15 17:29:31 +0900

    Add Controller

---
+++ src/main/java/messagesource/messagesourcespring/controller/MessagesourceSpringController.java
@@ -0,0 +1,13 @@
+package messagesource.messagesourcespring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessagesourceSpringController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
