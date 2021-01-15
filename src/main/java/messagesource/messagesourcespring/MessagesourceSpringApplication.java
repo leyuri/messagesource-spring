050a5eccdfa883926317fcaba323ab499bd61ccc
leyuri <leyuri97@gmail.com>
2021-01-15 17:25:55 +0900

    First commit

---
+++ src/main/java/messagesource/messagesourcespring/MessagesourceSpringApplication.java
@@ -0,0 +1,13 @@
+package messagesource.messagesourcespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesourceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagesourceSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/messagesource/messagesourcespring/MessagesourceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package messagesource.messagesourcespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesourceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
